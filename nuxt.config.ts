8d2f1c3c58803f6498cdbaf6ace84a32f8fcd760
Daniel Roe <daniel@roe.dev>
2022-08-01 14:21:16 +0100

    chore: expose internal server

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,21 @@
-import { defineNuxtConfig } from "nuxt";
+import { cp } from 'fs/promises';
+import { defineNuxtConfig } from 'nuxt';
+import { fileURLToPath } from 'url';
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   typescript: {
     shim: false,
+  },
+  hooks: {
+    'nitro:init': (nitro) => {
+      nitro.hooks.hook('compiled', async () => {
+        const internalURL = fileURLToPath(
+          new URL('./.netlify', import.meta.url),
+        );
+        const destination = fileURLToPath(new URL('dist', import.meta.url));
+        await cp(internalURL, destination, { recursive: true });
+      });
+    },
   },
 });
