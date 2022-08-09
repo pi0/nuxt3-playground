515c8712a8fb8fa7514d50d605e60b1f9980c24f
Pooya Parsa <pooya@pi0.io>
2022-08-09 17:51:41 +0200

    update deps and enable experimental flag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,25 +1,8 @@
 import { defineNuxtConfig } from 'nuxt'
-import { execaCommand } from 'execa'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  hooks: {
-    "nitro:init"(nitro) {
-      nitro.hooks.hook('compiled', async () => {
-        // await execaCommand('tar -cvzf dist/app.tgz --exclude node_modules --exclude .git --exclude dist/app.tgz .')
-      })
-    },
-    'vite:extendConfig'(config, { isServer }) {
-      if (isServer) {
-        // config.build.rollupOptions.output.inlineDynamicImports = true
-      }
-    }
-  },
-  nitro: {
-    rollupConfig: {
-      output: {
-        entryFileNames: "server.mjs"
-      }
-    },
+  experimental: {
+    viteServerDynamicImports: true
   }
 })
